10f14bb15a6b7244d01a9e1226f9689ca7a23c62
Vedant Bassi <bassi.vdt@gmail.com>
2018-02-19 21:29:31 +0530

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', process.env.PORT || 3001);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
